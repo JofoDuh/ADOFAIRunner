2bc10b56e5bac8bb0a19ee047bb1152a9e09105a
Jofo <kienthucquybau@gmail.com>
2025-08-31 00:15:41 +0200

    spelling mistake 💔

--- Editor/Core/RunLogic.cs
+++ Editor/Core/RunLogic.cs
@@ -53,7 +53,7 @@ namespace ADOFAIRunner.Core
                     return;
                 }
 
-                string modName = manifest.Identity.name;
+                string modName = manifest.Identity.Name;
                 string baseModPath = build switch
                 {
                     BuildTarget.Auto => buildType == "BEPINEX" ? settings.BepInExModFolderPath : settings.UMMModFolderPath,
